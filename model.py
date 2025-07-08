f4bea19f19f4d7aa64349d7798360c7fb0953c58
princ0301 <prince07190@gmail.com>
2025-07-09 00:25:51 +0530

    audio classifier cnn

--- model.py
+++ model.py
@@ -1,22 +1,22 @@
 import torch
 import torch.nn as nn
 
+
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, 3, 
-                               stride, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(in_channels, out_channels,
+                               3, stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, 3,
-                               stride, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(out_channels, out_channels,
+                               3, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channels)
 
         self.shortcut = nn.Sequential()
         self.use_shortcut = stride != 1 or in_channels != out_channels
         if self.use_shortcut:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, 1, stride, bias=False), nn.BatchNorm2d(out_channels)
-            )
+                nn.Conv2d(in_channels, out_channels, 1, stride=stride, bias=False), nn.BatchNorm2d(out_channels))
 
     def forward(self, x, fmap_dict=None, prefix=""):
         out = self.conv1(x)
@@ -35,25 +35,21 @@ class ResidualBlock(nn.Module):
             fmap_dict[f"{prefix}.relu"] = out
 
         return out
-    
+
+
 class AudioCNN(nn.Module):
     def __init__(self, num_classes=50):
+        super().__init__()
         self.conv1 = nn.Sequential(
-            nn.Conv2d(1, 64, 7, stride=2, padding=3, bias=False), 
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(3, stride=2, padding=1)
-        )
-
+            nn.Conv2d(1, 64, 7, stride=2, padding=3, bias=False), nn.BatchNorm2d(64), nn.ReLU(inplace=True), nn.MaxPool2d(3, stride=2, padding=1))
         self.layer1 = nn.ModuleList([ResidualBlock(64, 64) for i in range(3)])
         self.layer2 = nn.ModuleList(
-            [ResidualBlock(64 if i == 0 else 128, 128, stride=2 if i == 0 else 1) for i in range(4)]
-        )
+            [ResidualBlock(64 if i == 0 else 128, 128, stride=2 if i == 0 else 1) for i in range(4)])
         self.layer3 = nn.ModuleList(
             [ResidualBlock(128 if i == 0 else 256, 256, stride=2 if i == 0 else 1) for i in range(6)])
         self.layer4 = nn.ModuleList(
             [ResidualBlock(256 if i == 0 else 512, 512, stride=2 if i == 0 else 1) for i in range(3)])
-        
+
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.dropout = nn.Dropout(0.5)
         self.fc = nn.Linear(512, num_classes)
